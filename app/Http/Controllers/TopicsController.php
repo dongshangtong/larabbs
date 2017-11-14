64e6c0d809887895b3912e6fcd3cfb94b7426482
dongshangtong <dongshangtong@163.com>
2017-11-14 02:48:35 +0000

    Prevent XSS injection

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -41,7 +41,7 @@ class TopicsController extends Controller
       // $topic->excerpt = make_excerpt($topic->body);
       $topic->save();
 
-      return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+      return redirect()->route('topics.show', $topic->id)->with('success', '成功创建主题！');
     }
 
 	public function edit(Topic $topic)
@@ -54,7 +54,7 @@ class TopicsController extends Controller
 	{
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('success', '更新成功！');
 	}
 
 	public function destroy(Topic $topic)
@@ -62,7 +62,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('topics.index')->with('success', '成功删除！');
 	}
 
 /**
--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -1,35 +1,5 @@
 @extends('layouts.app')
-
-@section('styles')
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
-@stop
-
-@section('scripts')
-    <script type="text/javascript"  src="{{ asset('js/module.js') }}"></script>
-    <script type="text/javascript"  src="{{ asset('js/hotkeys.js') }}"></script>
-    <script type="text/javascript"  src="{{ asset('js/uploader.js') }}"></script>
-    <script type="text/javascript"  src="{{ asset('js/simditor.js') }}"></script>
-
-    <script>
-    $(document).ready(function(){
-       var editor = new Simditor({
-           textarea: $('#editor'),
-           upload: {
-               url: '{{ route('topics.upload_image') }}',
-               params: { _token: '{{ csrf_token() }}' },
-               fileKey: 'upload_file',
-               connectionCount: 3,
-               leaveConfirm: '文件上传中，关闭此页面将取消上传。'
-           },
-           pasteImage: true,
-       });
-   });
-    </script>
-
-@stop
-
 @section('content')
-
 <div class="container">
     <div class="col-md-10 col-md-offset-1">
         <div class="panel panel-default">
@@ -82,5 +52,32 @@
         </div>
     </div>
 </div>
-
 @endsection
+
+
+@section('styles')
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
+@stop
+
+@section('scripts')
+    <script type="text/javascript"  src="{{ asset('js/module.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/hotkeys.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/uploader.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/simditor.js') }}"></script>
+
+    <script>
+    $(document).ready(function(){
+       var editor = new Simditor({
+           textarea: $('#editor'),
+           upload: {
+               url: '{{ route('topics.upload_image') }}',
+               params: { _token: '{{ csrf_token() }}' },
+               fileKey: 'upload_file',
+               connectionCount: 3,
+               leaveConfirm: '文件上传中，关闭此页面将取消上传。'
+           },
+           pasteImage: true,
+       });
+   });
+    </script>
+@stop
