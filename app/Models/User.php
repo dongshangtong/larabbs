bf5f8cfe0ec1b6be9c7f21adf0ac17339e92f56a
dongshangtong <dongshangtong@163.com>
2017-11-14 15:20:47 +0000

    List user replies

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,4 +37,10 @@ class User extends Authenticatable
    {
        return $this->id == $model->user_id;
    }
+
+
+   public function replies()
+  {
+      return $this->hasMany(Reply::class);
+  }
 }
--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -2,22 +2,20 @@
 
 namespace App\Observers;
 
-use App\Models\User;
+use App\Models\Reply;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
-class UserObserver
+class ReplyObserver
 {
-    public function creating(User $user)
+    public function creating(Reply $reply)
     {
         //
     }
 
-    public function updating(User $user)
+    public function updating(Reply $reply)
     {
         //
     }
-
-
 }
