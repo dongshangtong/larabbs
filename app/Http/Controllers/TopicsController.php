5af6d97adf380f1c9e02981e258dcc8ce8417c1b
dongshangtong <dongshangtong@163.com>
2017-11-15 08:26:19 +0000

    Sidebar active users

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -10,6 +10,7 @@ use App\Http\Requests\TopicRequest;
 use App\Models\Category;
 use App\Handlers\ImageUploadHandler;
 use Auth;
+use App\Models\User;
 class TopicsController extends Controller
 {
     public function __construct()
@@ -17,10 +18,14 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-    public function index(Request $request, Topic $topic)
+    public function index(Request $request, Topic $topic, User $user)
     {
-     $topics = $topic->withOrder($request->order)->paginate(20);
-     return view('topics.index', compact('topics'));
+      $topics = $topic->withOrder($request->order)->paginate(20);
+      $active_users = $user->getActiveUsers();
+
+      //dd()可以在页面打印。第一次发现。
+     // dd($active_users);
+      return view('topics.index', compact('topics', 'active_users'));
     }
 
     public function show( Request $request,Topic $topic)
